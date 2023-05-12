71d3d459c88e9cbae08b187622e1222f585cafb2
TrickEye <trickeye@buaa.edu.cn>
2023-05-12 17:12:31 +0800

    Deployed 422cb5d with MkDocs version: 1.4.2

--- UB41to60/test.c
+++ UB41to60/test.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 
 int main() {
-    char str[] = "hello world";
-    str[2] = 'x';
-    printf("%s", str);
+    char str = "hello world";
+    char str1[] = "hello world";
+    // str[2] = 'x';
+    // printf("%s", str);
+    if (str1 == str) {
+        printf("EQL");
+    } else {
+        printf("NEQ");
+    }
 }
