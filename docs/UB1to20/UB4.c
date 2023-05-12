6c5f1fa6141c9a7fcd27ff720f45a3866bad995f
894561315 <894561315@qq.com>
2023-05-12 13:21:35 +0800

    推送UB4,UB26,UB48

---
+++ docs/UB1to20/UB4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    int a哈 =1;
+    printf("%d\n",a哈);
+    return 0;
+}
---
+++ docs/UB21to40/UB26.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "string.h
+
+
+int main()
+{
+    printf("Hello");
+    return 0;
+}
